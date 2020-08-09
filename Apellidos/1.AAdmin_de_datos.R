4c1392870d28e5b8b9f6586288a08fa5297f1dd5
FranVasEstay <francisca.vasquez.e@ug.uchile.cl>
2020-08-09 19:42:42 -0400

    actualización de script

--- Apellidos/1.AAdmin_de_datos.R
+++ Apellidos/1.AAdmin_de_datos.R
@@ -1,16 +1,42 @@
 ###Limpieza de base de datos
 
 library(dplyr)
+library(ade4)
+library(tidyr)
+
 
 #Working directory
 getwd()
 setwd("C:/Users/Pruebas/Documents/Practica/DIRECTORIO_PROYECTO/Datos/Comun")
 
-#SELECCION DE DATOS ÚTILES (community, surname_father)
+#SELECCION DE DATOS ÚTILES: comuna de coquimbo, variables(province, surname_father)
 commoners <- read.csv("commoners.csv", sep=",", header=TRUE, fill = TRUE)
 data.frame(names(commoners))
-surnames<-dplyr::select(commoners, community,surname_father)  
+surnames<-dplyr::select(commoners,region,province,surname_father)  
 data.frame(names(surnames))
-surnames %>%
-group_by(community, add = FALSE) 
+surnames <- subset(surnames, region == "COQUIMBO") 
+surnames<-dplyr::select(surnames,province,surname_father)
+data.frame(names(surnames))
 
+### CREACIÓN DE BASE DE DATOS PARA ÁRBOLES POR PROVINCIA 
+
+#AGRUPACIÓN POR PROVINCIA y CONTABILIZACIÓN DE APELLIDOS
+surnames %>%
+group_by(province,surname_father)%>%
+summarise(n()) -> X
+
+X %>%
+  pivot_wider(names_from = "surname_father", values_from = "n()") -> FINAL
+FINAL[is.na(FINAL)] <-0
+
+#CREACIÓN DE MATRIZ CSV CON ESOS DATOS
+setwd("C:/Users/Pruebas/Documents/Practica/DIRECTORIO_PROYECTO/Archive")
+write.csv(FINAL,"frecuencia_apellidos_prov.csv")
+
+#BASE BINARIA (PRESENCIA 1 -AUSENCIA 0)
+FINAL[FINAL > 0] <- 1
+
+#CREACIÓN DE MATRIZ CSV CON ESOS DATOS
+write.csv(FINAL,"frecuencia_apellidos_prov_binario.csv")
+
+#CREACIÓN DE ARCHIVO NEXUS CON ESOS DATOS
